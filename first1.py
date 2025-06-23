0c501226a999f69d46b9df05a29a513279fc0abb
Luluh <lulualmogbil@gmail.com>
2025-06-24 00:29:28 +0300

    Temp

--- first1.py
+++ first1.py
@@ -4,7 +4,7 @@
 
 
 ## Below you have a code that raises an exception , using what you learned do the following:
-- Find what type of exception is raised.
+- Find what type of exception is raised. Name error
 - Hanlde the exception in try..except 
 - If operation successful , print "the operation is successful"
 - if operation fails, handle the specific exception that is raised , and print a relevant message.
@@ -18,13 +18,49 @@ def additoin(x, y):
 additoin(10, 20)
 '''
 
-def additoin(x, y):
-    try:
-        x = 10
-        y = 20
-        print("Addition:", x + b)  # 'b' is not defined, this will raise NameError
-        print("the operation is successful")
-    except NameError:
-        print("error: used a variable that isn't defined (NameError).")
 
-additoin(10, 20)
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+
+
+try:
+    additoin(10, 20)
+    print("the operation is successful")
+except NameError as e:
+    print("This is a name error exception, vars are not compatable")
+except Exception as e:
+    print(e.__class__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def additoin(x, y):
+#     try:
+#         x = 10
+#         y = 20
+#         print("Addition:", x + b)  # 'b' is not defined, this will raise NameError
+#         print("the operation is successful")
+#     except NameError:
+#         print("error: used a variable that isn't defined (NameError).")
+
+# additoin(10, 20)
+
+
+
+
+
+
